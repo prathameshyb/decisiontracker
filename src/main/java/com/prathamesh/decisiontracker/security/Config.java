5db9ad84dc1544161dbb581fba1a2f127232efc6
Prathamesh Y Bhadane <prathameshbhadane496@gmail.com>
2026-08-16 19:03:20 +0530

    Adding memory based auth

--- src/main/java/com/prathamesh/decisiontracker/security/Config.java
+++ src/main/java/com/prathamesh/decisiontracker/security/Config.java
@@ -2,12 +2,21 @@ package com.prathamesh.decisiontracker.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Config {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
